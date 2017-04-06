976f64d856dbf1a365d052508301bb437f22351c
Robert Jin <robert.jin@vgbio.local>
2017-04-05 22:39:13 -0500

    Added get request in home, storing image data from server to notes var

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,21 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 import { AlertController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+
+import { dataSrvc } from './data.service';
+
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	notes: Array<string> = [];
+	posts: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http, public plt: Platform) {
+
+  	this.plt.ready().then((readySource) => {
+      console.log('Platform ready from', readySource);
+      this.http.get("http://sebastianperez.pythonanywhere.com/api/v1/notes")
+      	.subscribe(data => {
+      		var res = data.json();
+      		this.notes.push(res["notes"]); // Stores notes data from get request in notes
+      		console.log(this.notes);
+      	});
+      
+    });
 
   }
 
+
+  // makeGetRequest = () : void => {
+  //     // this.navCtrl.setRoot(UploadingPage, {
+  //     //     images: this.images
+  //     // });
+  //     console.log(this.test);
+  //     }  
+
+
+
+
+	// ngOnInit() {
+ //        return this.service.getComments().then(data => this.comments = data);
+ //    }
+
+
+
 }
+
+// $scope.doThisFunction = function(){
+//     document.getElementById("test").innerHTML = "AJOOAOA";
+// };
+
+
+
 
 
 
