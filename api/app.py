47b47ede9a93c1bd3022fdf62622d61de54d22af
sgp715 <7sebastianperez@gmail.com>
2017-04-06 11:10:21 -0500

    starting sqlite

--- api/app.py
+++ api/app.py
@@ -8,9 +8,26 @@ import uuid
 
 import base64
 
+import sqlite3
+
 
 app = Flask(__name__)
 api = Api(app)
+
+# database initialization stuff
+DATABASE = './database/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 class Notes(Resource):
