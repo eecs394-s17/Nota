f93293bcef1355cc106b3d4d54eb0a64e620a0ca
sgp715 <7sebastianperez@gmail.com>
2017-04-07 10:05:46 -0500

    adding a file to initialize a simple db

---
+++ api/start.py
@@ -0,0 +1,21 @@
+import sqlite3
+import os
+
+
+conn = sqlite3.connect('database/database.db')
+c = conn.cursor()
+
+
+# make the database dir if it doesn't exist
+try:
+    os.stat("database")
+except:
+    os.mkdir("database")
+
+# try to create the notes table
+table_description = "CREATE TABLE notes (note_path text, class text, lecture text, price real, name text)"
+try:
+    c.execute(table_description)
+except:
+    print "Could not create table"
+    exit()
