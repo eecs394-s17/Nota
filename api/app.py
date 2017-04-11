d35f444654ce090225bd509a55a93b9af723dd61
brianzhan <brian.zhan@u.northwestern.edu>
2017-04-11 14:30:34 -0500

    login API working

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_restful import Resource, Api, reqparse
 from flask import g
 from flask import abort
+
 
 import datetime
 
@@ -11,9 +12,55 @@ import base64
 
 import sqlite3
 
+from flask.ext.login import LoginManager, UserMixin, login_required
+
 
 app = Flask(__name__)
 api = Api(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    # proxy for a database of users
+    user_database = {"JohnDoe": ("JohnDoe", "John"),
+               "JaneDoe": ("JaneDoe", "Jane")}
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
+
+
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username,password = token.split(":") # naive token
+        user_entry = User.get(username)
+        if (user_entry is not None):
+            user = User(user_entry[0],user_entry[1])
+            if (user.password == password):
+                return user
+    return None
+
+
+@app.route("/",methods=["GET"])
+def index():
+    return Response(response="Hello World!",status=200)
+
+
+@app.route("/protected/",methods=["GET"])
+@login_required
+def protected():
+    return Response(response="Hello Protected World!", status=200)
+
+
 
 
 # database initialization stuff
@@ -145,4 +192,6 @@ api.add_resource(Notes, "/api/v1/notes")
 
 
 if __name__ == '__main__':
+    app.config["SECRET_KEY"] = "ITSASECRET"
+    # app.run(port=5000,debug=True)
     app.run(debug=True)
