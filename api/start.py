d11830409968d60d44efd76377bd1d4bc71ebb74
Robert Jin <dajunjin2016@u.northwestern.edu>
2017-04-12 10:30:05 -0500

    unique constraint on filename

--- api/start.py
+++ api/start.py
@@ -13,7 +13,7 @@ except:
     os.mkdir("database")
 
 # try to create the notes table
-notes_table = "CREATE TABLE notes (id integer primary key autoincrement, filename text, upload_date text, course text, title text, price text, description text, userID integer, foreign key(userID) references users(id))"
+notes_table = "CREATE TABLE notes (id integer primary key autoincrement, filename text, upload_date text, course text, title text, price text, description text, userID integer, constraint unique_filename UNIQUE (filename), foreign key(userID) references users(id))"
 users_table = "CREATE TABLE users(id integer primary key autoincrement, username text unique, email text, password text, constraint unique_username UNIQUE (username), constraint unique_email UNIQUE (email))"
 
 
