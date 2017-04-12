0a170a86f76d620c489a0dd2521b902d88039802
Robert Jin <dajunjin2016@u.northwestern.edu>
2017-04-12 10:28:38 -0500

    added unique constraints on username and email

--- api/start.py
+++ api/start.py
@@ -14,7 +14,7 @@ except:
 
 # try to create the notes table
 notes_table = "CREATE TABLE notes (id integer primary key autoincrement, filename text, upload_date text, course text, title text, price text, description text, userID integer, foreign key(userID) references users(id))"
-users_table = "CREATE TABLE users(id integer primary key autoincrement, username text, email text, password text)"
+users_table = "CREATE TABLE users(id integer primary key autoincrement, username text unique, email text, password text, constraint unique_username UNIQUE (username), constraint unique_email UNIQUE (email))"
 
 
 
