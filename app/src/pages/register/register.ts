40f64ede5ba85126d0ee4e37db084e48b45ba10b
chankyu oh <chankyuoh@u.northwestern.edu>
2017-04-13 20:56:23 -0500

    more fields in sign up

--- app/src/pages/register/register.ts
+++ app/src/pages/register/register.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
- 
+import {TabsPage} from "../tabs/tabs";
+
+import { Http } from '@angular/http';
+
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html'
 })
 export class RegisterPage {
   createSuccess = false;
-  registerCredentials = {email: '', password: ''};
- 
-  constructor(private nav: NavController, private auth: AuthService, private alertCtrl: AlertController) {}
- 
+  registerCredentials = {firstName: '', lastName: '', email: '', password: ''};
+
+  constructor(public http: Http, private nav: NavController, private auth: AuthService, private alertCtrl: AlertController) {}
+
   public register() {
     this.auth.register(this.registerCredentials).subscribe(success => {
       if (success) {
         this.createSuccess = true;
+        this.makePostRequest();
           this.showPopup("Success", "Account created.");
+          this.nav.push(TabsPage);
+
       } else {
         this.showPopup("Error", "Problem creating account.");
       }
@@ -25,7 +31,7 @@ export class RegisterPage {
       this.showPopup("Error", error);
     });
   }
- 
+
   showPopup(title, text) {
     let alert = this.alertCtrl.create({
       title: title,
@@ -35,12 +41,38 @@ export class RegisterPage {
          text: 'OK',
          handler: data => {
            if (this.createSuccess) {
-             this.nav.popToRoot();
+             //this.nav.popToRoot();
            }
          }
        }
      ]
     });
     alert.present();
+  }
+
+  makePostRequest() {
+    var data1 = {
+      'firstName': this.registerCredentials.firstName,
+      'lastName': this.registerCredentials.lastName,
+      'email': this.registerCredentials.email,
+      'password': this.registerCredentials.password
+    };
+
+    console.log(data1);
+
+
+    // this.http.post("http://127.0.0.1:5000/api/v1/notes", data1)
+
+    this.http.post("http://34.209.98.85:5000/api/v1/notes", data1)
+      .subscribe(data => {
+        // var alert = Alert.create({
+        //     title: "Data String",
+        //     subTitle: data.json().data,
+        //     buttons: ["close"]
+        // });
+        // this.nav.present(alert); // I guess this is deprecated line, see http://stackoverflow.com/questions/41932399/ionic2-property-present-does-not-exist-on-type-navcontroller
+      }, error => {
+        console.log(JSON.stringify(error.json()));
+      });
   }
 }
