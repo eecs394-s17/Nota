8c1846ec23e690ea4ea584a6e71c640d77baddf7
sgp715 <7sebastianperez@gmail.com>
2017-04-13 23:20:53 -0500

    returning id of notes inserted on post

--- api/app/notes.py
+++ api/app/notes.py
@@ -14,14 +14,7 @@ import base64
 class Notes(Resource):
 
     def __init__(self):
-
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument("title", type=str, required=True)
-        self.parser.add_argument("course", type=str, required=True)
-        self.parser.add_argument("price", type=str, required=True)
-        self.parser.add_argument("notes", type=str, required=True)
-        self.parser.add_argument("description", type=str, required=False)
-        self.parser.add_argument("id")
+        pass
 
     def get(self):
         """
@@ -30,30 +23,68 @@ class Notes(Resource):
         2) /api/v1/notes/{id} -> this will return the metadata as well as the notes themselves
         TODO: eventually we will wanna abstract this out so that metadata is a different endpoint
         """
+
         notes = []
 
         conn = get_db()
         conn.row_factory = dict_factory
         c = conn.cursor()
 
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", type=str, required=False, location="args")
+        args = parser.parse_args()
+
+        # if we have indeed found and id let's get the corresponding notes assuming they exist
+        notes_id = args["id"]
+        # if notes_id != None:
+        #
+        #     notes_iterator = c.execute("SELECT * FROM notes WHERE id='" + notes_id + "' LIMIT 1")
+        #
+        #     with open(row["filename"], "rb") as f:
+        #
+        #         data = f.read()
+        #         notes_data = base64.encodestring(data)
+        #
+        #         current_notes = {
+        #                         "id": row["id"],
+        #                         "course": row["course"],
+        #                         "upload_date": row["upload_date"],
+        #                         "price": row["price"],
+        #                         "title": row["title"],
+        #                         "description": row["description"],
+        #                         "user_id": row["userID"],
+        #                         "notes": notes_data
+        #                         }
+
+        # otherwise we are gonna return all the notes
         for row in c.execute("SELECT * FROM notes"):
 
-            with open(row["filename"], "rb") as f:
+            # with open(row["filename"], "rb") as f:
+            #
+            #     data = f.read()
+            #     notes_data = base64.encodestring(data)
+            #
+            #     current_notes = {
+            #                     "notes": notes_data,
+            #                     "course": row["course"],
+            #                     "upload_date": row["upload_date"],
+            #                     "price": row["price"],
+            #                     "title": row["title"],
+            #                     "description": row["description"],
+            #                     "user_id": row["userID"]
+            #                     }
 
-                data = f.read()
-                notes_data = base64.encodestring(data)
+            current_notes = {
+                            "id": row["id"],
+                            "course": row["course"],
+                            "upload_date": row["upload_date"],
+                            "price": row["price"],
+                            "title": row["title"],
+                            "description": row["description"],
+                            "user_id": row["userID"]
+                            }
 
-                current_notes = {
-                                "notes": notes_data,
-                                "course": row["course"],
-                                "upload_date": row["upload_date"],
-                                "price": row["price"],
-                                "title": row["title"],
-                                "description": row["description"],
-                                "userID": row["userID"]
-                                }
-
-                notes.append(current_notes)
+            notes.append(current_notes)
 
         notes_json = { "all_notes" : notes }
 
@@ -66,7 +97,13 @@ class Notes(Resource):
         1) /api/v1/notes -> creates a new set of notes
         """
 
-        args = self.parser.parse_args()
+        parser = reqparse.RequestParser()
+        parser.add_argument("title", type=str, required=True)
+        parser.add_argument("course", type=str, required=True)
+        parser.add_argument("price", type=str, required=True)
+        parser.add_argument("notes", type=str, required=True)
+        parser.add_argument("description", type=str, required=False)
+        args = parser.parse_args()
 
         course = args["course"]
         upload_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
@@ -75,7 +112,7 @@ class Notes(Resource):
         notes = args["notes"]
         description = args["description"]
         userID = 5
-        username = "dajunjin"
+        username = "test_user"
 
 
         if description == None:
@@ -85,7 +122,8 @@ class Notes(Resource):
         image = base64.decodestring(notes)
 
         # save base64 to somewhere on machine
-        unique_filename = "/srv/images/" + str(uuid.uuid4())
+        # unique_filename = "/srv/images/" + str(uuid.uuid4()) # eventually have this run when not in debug
+        unique_filename = str(uuid.uuid4())
         with open(unique_filename, 'wb') as f:
             f.write(image)
 
@@ -98,7 +136,9 @@ class Notes(Resource):
         c.execute("INSERT INTO notes (filename, upload_date, course, title, price, description, userID) VALUES (?,?,?,?,?,?,?)", (unique_filename, upload_date, course, title, price, description, userID))
         conn.commit()
 
-        return { "id" : unique_filename, "user_id": userID }
+        last_row_id = c.lastrowid
+
+        return { "id" : last_row_id }
 
     def delete(self):
         """
