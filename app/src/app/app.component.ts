9c52326d33d6ee781fee84f3587d04d65aaf2da1
chankyu oh <chankyuoh@u.northwestern.edu>
2017-04-13 20:16:55 -0500

    set register page as root page

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
+import {RegisterPage} from "../pages/register/register";
 
 
 @Component({
@@ -12,10 +13,10 @@ import { LoginPage } from '../pages/login/login';
 })
 export class MyApp {
   // rootPage:any = LoginPage;
-  rootPage:any = TabsPage;
+  rootPage:any = RegisterPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => { 
+    platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
