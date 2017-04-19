c57fb52c1cf6f459a5dec018aa471f450a4f95f9
Robert Jin <dajunjin2016@u.northwestern.edu>
2017-04-18 19:53:34 -0500

    fixed image path

--- app/src/pages/about/about.ts
+++ app/src/pages/about/about.ts
@@ -25,8 +25,8 @@ export class AboutPage {
 		  title: ["", Validators.required],
     	course: ["", Validators.required], //validator not working because submit button fires post function on click
     	description: ["", Validators.required],
-    	price: ["", Validators.compose([Validators.pattern('^[0-9]+$'),Validators.required])],
-      images: [[], Validators.required]
+    	price: ["", Validators.compose([Validators.pattern('^[0-9]+$'),Validators.required])]
+      // images: [[], Validators.required]
 	});
 	uploadingPage = UploadingPage;
 
