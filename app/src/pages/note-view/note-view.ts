47eb2a90e0d661b263d950a1cbbd0d40aedfaf63
Robert Jin <dajunjin2016@u.northwestern.edu>
2017-04-22 15:33:59 -0500

    Changes to note-view for downloading onto phone

--- app/src/pages/note-view/note-view.ts
+++ app/src/pages/note-view/note-view.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
+import { NavController, NavParams, Platform, AlertController } from 'ionic-angular';
 
 import { Http } from '@angular/http';
 
-// import {Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
-import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
+import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
+import { File } from '@ionic-native/file';
+
+// import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 
 import { DomSanitizer } from '@angular/platform-browser';
 
 
 import 'rxjs/add/operator/map'
 
+declare var FileUploadOptions: any;
 
-declare var cordova: any;
+// declare var cordova: any;
 /*
   Generated class for the NoteView page.
 
@@ -22,8 +25,7 @@ declare var cordova: any;
 @Component({
   selector: 'page-note-view',
   templateUrl: 'note-view.html',
-  providers: [Base64ToGallery]
-
+  providers: [File, Transfer, TransferObject]
 })
 export class NoteViewPage {
   item: {title: string, course: string, upload_date: string, price: string, note: string, description: string};
@@ -37,7 +39,7 @@ export class NoteViewPage {
   base64:string = "";
 
 
-  constructor(private _domSanitizer: DomSanitizer, private base64ToGallery: Base64ToGallery, public navCtrl: NavController, public navParams: NavParams, public plt: Platform, public http: Http) {
+  constructor(private _domSanitizer: DomSanitizer, public alertCtrl: AlertController, private file: File, private transfer: Transfer, public navCtrl: NavController, public navParams: NavParams, public plt: Platform, public http: Http) {
 //     this.plt.ready().then((readySource) => {
 //       console.log('Platform ready from', readySource);
 //       this.http.get("http://127.0.0.1:5000/api/v1/notes")
@@ -55,13 +57,54 @@ export class NoteViewPage {
 
 
 
-  download(){
-    this.base64ToGallery.base64ToGallery(this.base64, { prefix: '_img', mediaScanner: true }).then(
-      res => console.log('Saved image to gallery ', res),
-      err => console.log('Error saving image to gallery ', err)
-    );
+  download() {
+    const fileTransfer = new TransferObject();
+    let targetPath;
+    // if(!this.platform.is('cordova')) {
+    //       return false;
+    // }
+    // if (this.platform.is('ios')) {
+      
+    targetPath = this.file.documentsDirectory + "yo.jpg";
+        // }
+    // else if(this.platform.is('android')) {
+    //       targetPath = this.file.dataDirectory + this.noteID;
+    // }
+    // console.log("encod   becomes ", encodeURI(atob(this.base64)));
+    var image = this.base64;
+    // var ext = image.split(';')[0].match(/jpeg|png|gif/)[0];
+    // var buf = new Buffer(image, 'base64');
+    // console.log("buf is ", buf);
+    // this.file.writeFile(this.file.documentsDirectory, "test.jpg", atob(image));
+    console.log(targetPath);
+    // console.log(this.noteID);
+    var url = "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png";
+    fileTransfer.download(url, targetPath);
 
-  }
+    fileTransfer.download(url, targetPath).then((entry) => {
+
+        const alertSuccess = this.alertCtrl.create({
+          title: `Download Succeeded!`,
+          subTitle: `${image} was successfully downloaded to: ${entry.toURL()}`,
+          buttons: ['Ok']
+        });
+
+        alertSuccess.present();
+
+      }, (error) => {
+
+        const alertFailure = this.alertCtrl.create({
+          title: `Download Failed!`,
+          subTitle: `${image} was not successfully downloaded. Error code: ${error.code}`,
+          buttons: ['Ok']
+        });
+
+        alertFailure.present();
+
+      });
+
+  
+}
 
 
   ionViewDidLoad() {
@@ -71,7 +114,7 @@ export class NoteViewPage {
     this.upload_date = this.navParams.get('upload_date');
     this.description = this.navParams.get('description');
     this.price = this.navParams.get('price');
-    this.note = this._domSanitizer.bypassSecurityTrustUrl("data:image/jpeg;base64," + this.navParams.get('note'));
+    // this.note = this._domSanitizer.bypassSecurityTrustUrl("data:image/jpeg;base64," + this.navParams.get('note'));
     this.noteID = this.navParams.get('noteID');
 
     this.http.get("http://34.209.98.85:5000/api/v1/notes" + "?id=" + this.noteID) 
