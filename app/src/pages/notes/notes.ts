0e6cdad99129d1d16d60ee28cd57be5d24399168
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-04-30 20:09:22 -0500

    search stuff and a little other

--- app/src/pages/notes/notes.ts
+++ app/src/pages/notes/notes.ts
@@ -15,130 +15,124 @@ import { NoteViewPage } from '../note-view/note-view';
 })
 // AKA NotesPage
 export class NotesPage {
-	notes: Array<string> = [];
-  items: Array<{title: string, course: string, upload_date: string, price: string, description: string, noteID: number, score: number}>;
-	posts: any;
+    notes: Array<string> = [];
+    items: Array<{title: string, course: string, upload_date: string, price: string, description: string, noteID: number, score: number}>;
+    search_string : string;
 
-  constructor(public navCtrl: NavController, public http: Http, public plt: Platform,) {
-    this.items = [];
-
-
-  	this.plt.ready().then((readySource) => {
-      console.log('Platform ready from', readySource);
-
-      // this.http.get("http://sebastianperez.pythonanywhere.com/api/v1/notes")
-      // 	.subscribe(data => {
-      // 		var res = data.json();
-      //     console.log(res);
-      // 		// this.items.push( { courseName: res['courseName'],}) etc.
-      //     var all_notes = res["all_notes"];
-      //     console.log(all_notes.length);
-      //     for (let i = 0; i< all_notes.length; i++){
-      //       console.log(all_notes[i]);
-      //       this.items.push({
-      //         title: all_notes[i].title,
-      //         course: all_notes[i].course,
-      //         upload_date: all_notes[i].upload_date,
-      //         price: all_notes[i].price,
-      //         note: all_notes[i].notes,
-      //         description: all_notes[i].description
-      //       });
-
-      //     }
+    constructor(public navCtrl: NavController, public http: Http, public plt: Platform,) {
+        this.items = [];
 
 
+        this.plt.ready().then((readySource) => {
+            console.log('Platform ready from', readySource);
+
+            // this.http.get("http://sebastianperez.pythonanywhere.com/api/v1/notes")
+            // 	.subscribe(data => {
+            // 		var res = data.json();
+            //     console.log(res);
+            // 		// this.items.push( { courseName: res['courseName'],}) etc.
+            //     var all_notes = res["all_notes"];
+            //     console.log(all_notes.length);
+            //     for (let i = 0; i< all_notes.length; i++){
+            //       console.log(all_notes[i]);
+            //       this.items.push({
+            //         title: all_notes[i].title,
+            //         course: all_notes[i].course,
+            //         upload_date: all_notes[i].upload_date,
+            //         price: all_notes[i].price,
+            //         note: all_notes[i].notes,
+            //         description: all_notes[i].description
+            //       });
+
+            //     }
 
 
-      // 	});
-
-    });
-
-
-
-  }
-
-
-  ionViewDidEnter() {
-    console.log('ionViewDidEnter b');
-    this.items = [];
-
-    // this.http.get("/api")
-   this.http.get("http://34.209.98.85:5000/api/v1/notes")
-        .subscribe(data => {
-          var res = data.json();
-          console.log(res);
-
-          // this.items.push( { courseName: res['courseName'],}) etc.
-          var all_notes = res["notes"];
-          console.log(all_notes.length);
-          for (let i = 0; i< all_notes.length; i++){
-            console.log(all_notes[i]);
-            this.items.push({
-              title: all_notes[i].title,
-              course: all_notes[i].course,
-              upload_date: all_notes[i].upload_date,
-              price: all_notes[i].price,
-              description: all_notes[i].description,
-              noteID: all_notes[i].id,
-              score: all_notes[i].score
-            });
-
-          }
+            // 	});
 
         });
-  }
-
-  launchNoteViewPage(item) {
-    console.log("note view page launch")
-    console.log(item)
-    this.navCtrl.push(NoteViewPage,item);
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(NotesPage, {
-      item: item
-    });
-  }
 
 
-  // makeGetRequest = () : void => {
-  //     // this.navCtrl.setRoot(UploadingPage, {
-  //     //     images: this.images
-  //     // });
-  //     console.log(this.test);
-  //     }
+    }
 
 
+    ionViewDidEnter() {
+        console.log('ionViewDidEnter b');
+        this.items = [];
+
+        // this.http.get("/api")
+        this.http.get("http://34.209.98.85:5000/api/v1/notes")
+            .subscribe(data => {
+                var res = data.json();
+                console.log(res);
+
+                // this.items.push( { courseName: res['courseName'],}) etc.
+                var all_notes = res["notes"];
+                console.log(all_notes.length);
+                for (let i = 0; i < all_notes.length; i++) {
+                    console.log(all_notes[i]);
+                    this.items.push({
+                        title: all_notes[i].title,
+                        course: all_notes[i].course,
+                        upload_date: all_notes[i].upload_date,
+                        price: all_notes[i].price,
+                        description: all_notes[i].description,
+                        noteID: all_notes[i].id,
+                        score: all_notes[i].score
+                    });
+
+                }
+
+            });
+    }
+
+    launchNoteViewPage(item) {
+        console.log("note view page launch")
+        console.log(item)
+        this.navCtrl.push(NoteViewPage, item);
+    }
+
+    itemTapped(event, item) {
+        // That's right, we're pushing to ourselves!
+        this.navCtrl.push(NotesPage, {
+            item: item
+        });
+    }
 
 
-	// ngOnInit() {
- //        return this.service.getComments().then(data => this.comments = data);
- //    }
+    // makeGetRequest = () : void => {
+    //     // this.navCtrl.setRoot(UploadingPage, {
+    //     //     images: this.images
+    //     // });
+    //     console.log(this.test);
+    //     }
 
 
+    // ngOnInit() {
+    //        return this.service.getComments().then(data => this.comments = data);
+    //    }
 
-}
 
 // $scope.doThisFunction = function(){
 //     document.getElementById("test").innerHTML = "AJOOAOA";
 // };
 
- search() {
-    // Declare variables
-    var input, filter, list, li, a, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    list = document.getElementById("myList");
-    li = list.getElementsByTagName('button');
+    search() {
+        // Declare variables
+        var input, filter, list, li, a, i;
+        input = document.getElementById('myInput');
+        filter = input.value.toUpperCase();
+        list = document.getElementById("myList");
+        li = list.getElementsByTagName('button');
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("h2")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("h2")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
         }
-    }
-};
+    };
+
+}
